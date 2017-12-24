e5bf008593e76d35facf01a352cd172849896346
Makerosophy <francesco.turlon@gmail.com>
2017-12-24 22:15:51 +0100

    Give some explanation on written code and remove the array from final log

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// This is the path of the file we want to convert
 const csvFilePath = 'customer-data.csv'
+// These are the modules we need
 const fs = require('fs')
 const csv = require('csvtojson')
+// We store data in the following array
 let arr = []
+
 csv()
   .fromFile(csvFilePath)
   .on('json',(jsonObj)=>{
@@ -9,10 +13,9 @@ csv()
   })
   .on('done',(error)=>{
     if (error) return process.exit(1)
-    console.log(arr)
     fs.writeFile('customer-data.json', JSON.stringify(arr, null, 2), (error)=>{
       if (error) return process.exit(1)
-      console.log('done')
+      console.log('Conversion is done! Please check the .json file in the folder')
       process.exit(0)
     })
   }
